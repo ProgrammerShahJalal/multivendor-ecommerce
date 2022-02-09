d4fc616f0e8048d4a66dd91c64d85fcc78696a70
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-02-09 13:37:50 +0600

    preject ready for code

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h2>We are Remote Coder Unity</h2>
-      <p>United we stand, divided we fall.</p>
-      <p>We all pull and merge code from development branch.</p>
+    <div className='text-center'>
+      <h2 className='text-5xl'>We are Remote Coder Unity</h2>
+      <p className="text-3xl">United we stand, divided we fall.</p>
+      <p className='text-green-500 text-3xl'>We all pull and merge code from development branch.</p>
     </div>
   );
 }
