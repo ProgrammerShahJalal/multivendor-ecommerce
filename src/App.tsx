8c11bd24a6ead365f9ac8b68422a2c0121d3e935
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-02-09 12:15:12 +0600

    development branch

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h2>We are Remote Coder Unity</h2>
       <p>United we stand, divided we fall.</p>
+      <p>We all pull and merge code from development branch.</p>
     </div>
   );
 }
