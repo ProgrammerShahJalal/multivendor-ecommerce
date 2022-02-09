7815c768b61eacbbfd1743775893f01438d4da62
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-02-09 22:22:23 +0600

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from './components/Shared/Header/Header';
 
 function App() {
   return (
-    <div className='text-center'>
-      <h2 className='text-5xl'>We are Remote Coder Unity</h2>
-      <p className="text-3xl">United we stand, divided we fall.</p>
-      <p className='text-green-500 text-3xl'>We all pull and merge code from development branch.</p>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+
+      </Routes>
+    </Router>
   );
 }
 
