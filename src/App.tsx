e7fd1fd062aa06aadf5066f7b1c5f80f25248bf7
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-02-11 01:12:10 +0600

    about completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Header from './components/Shared/Header';
 import Footer from './components/Shared/Footer/Footer';
+import Contact from './components/Contact/Contact';
+import NotFound from './components/Contact/Contact';
+import About from "./components/Pages/About/About";
 
 function App() {
   return (
@@ -10,7 +13,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/home' element={<Home />} />
-
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
       </Routes>
       <Footer />
     </Router>
