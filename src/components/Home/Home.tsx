ab1d8ecc276c1370a65afc0686abc8c0f9bf8186
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-02-10 20:01:56 +0600

    header footer

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import HomeProducts from '../HomeProducts/HomeProducts'
+import HomeProducts from '../HomeProducts/HomeProducts';
 
 export default function Home() {
     return (
