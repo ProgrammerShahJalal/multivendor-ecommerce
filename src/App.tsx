60e43a8e63571e5c69ba0743d31dadeade7f66d8
unknown <freelancerfathe@gmail.com>
2022-02-11 10:26:01 +0600

    navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Navigation from "./components/Shared/Navigation/Navigation";
 
 function App() {
   return (
     <Router>
+      <Navigation />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/home' element={<Home />} />
