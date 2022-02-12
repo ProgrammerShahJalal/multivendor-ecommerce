93c7f25cc8ecbbd2b400c068c15c60aa22a96851
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-02-12 17:35:17 +0600

    delete ifaz issue

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import HomeProducts from '../HomeProducts/HomeProducts';
 import './Home.css';
 import Promo from '../Promo/Promo';
 
 export default function Home() {
     return (
         <div>
-            <HomeProducts />
             <Promo />
         </div>
     )
