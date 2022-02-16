efdb6874cbceecc7d82b3f861db554d8af9caeca
saharabanu <saharabanungn@gmail.com>
2022-02-16 15:30:19 +0600

    checkout page added

--- src/components/Pages/Team/Team.tsx
+++ src/components/Pages/Team/Team.tsx
@@ -9,8 +9,9 @@ export default class App extends React.PureComponent<IAppProps> {
     return (
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 bg-red-100">
         <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-          <div>
-            <h1 className="inline-block px-3 py-px mb-4 text-2xl font-semibold tracking-wider text-orange-900 uppercase rounded-full bg-teal-accent-400">
+          <div className=''>
+            
+          <h1 className="inline-block px-3 py-px mb-4 text-2xl font-semibold tracking-wider text-orange-900 uppercase rounded-full bg-teal-accent-400">
              Our Team
             </h1>
           </div>
