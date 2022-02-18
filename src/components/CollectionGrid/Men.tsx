363fbdeb8846c4039edf5f715097d4cfb3b997b6
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-02-18 18:38:06 +0600

    image compress

--- src/components/CollectionGrid/Men.tsx
+++ src/components/CollectionGrid/Men.tsx
@@ -25,42 +25,42 @@ const Men = () => {
         }
     }, [products])
 
-return (
-    <div className="container px-10 pb-16">
-    <h2 className="text-2xl font-medium text-gray-800 uppercase mb-6">Top new Arrival</h2>
-    <div className="grid lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-1 gap-6">
-        {
-            products.map((product) => (
-                <div className="bg-white shadow-inner overflow-hidden group">
-                <div className="relative">
-                    <img style={{height:'300px'}}  src={product.img} className='w-full' alt="" />
-                    <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
-                        <a className='text-white text-lg w-9 h-8 rounded-full bg-red-600 flex items-center justify-center hover:bg-gray-800 transition' href="/">
-                        <i className="fa-regular fa-magnifying-glass"></i>
-                        </a>
-                        <a className='text-white text-lg w-9 h-8 rounded-full bg-red-600 flex items-center justify-center hover:bg-gray-800 transition' href="/">
-                        <i className="fa-regular fa-heart"></i>
-                        </a>
-                    </div>
-                   
-                </div>
-                <div className="pt-4 gb-3 px-4">
-                        <a href="/">
-                            <h4 className="font-medium text-xl mb-2 text-grey-800 hover:text-red-600 transition">{product.title}</h4>
-                            <h5 className="font-bold text-sm mb-2 text-grey-800 hover:text-red-600 transition">from {product.vendorName}</h5>
-                        </a>
-                        <div className="flex items-baseline mb-1 space-x-2">
-                        <p className="text-xl text-red-600 font-semibold">{product.salePrice}</p>
-                            <p className="text-sm text-gray-400 line-through">{product.price}</p>
+    return (
+        <div className="container px-10 pb-16">
+            <h2 className="text-2xl font-medium text-gray-800 uppercase mb-6">Top new Arrival</h2>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 xs:grid-cols-1 gap-6">
+                {
+                    products.map((product) => (
+                        <div className="bg-white shadow-inner overflow-hidden group">
+                            <div className="relative">
+                                <img style={{ height: '300px' }} src={product.img} className='w-full' alt="" />
+                                <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center gap-2 opacity-0 group-hover:opacity-100 transition">
+                                    <a className='text-white text-lg w-9 h-8 rounded-full bg-red-600 flex items-center justify-center hover:bg-gray-800 transition' href="/">
+                                        <i className="fa-regular fa-magnifying-glass"></i>
+                                    </a>
+                                    <a className='text-white text-lg w-9 h-8 rounded-full bg-red-600 flex items-center justify-center hover:bg-gray-800 transition' href="/">
+                                        <i className="fa-regular fa-heart"></i>
+                                    </a>
+                                </div>
+
+                            </div>
+                            <div className="pt-4 gb-3 px-4">
+                                <a href="/">
+                                    <h4 className="font-medium text-xl mb-2 text-grey-800 hover:text-red-600 transition">{product.title}</h4>
+                                    <h5 className="font-bold text-sm mb-2 text-grey-800 hover:text-red-600 transition">from {product.vendorName}</h5>
+                                </a>
+                                <div className="flex items-baseline mb-1 space-x-2">
+                                    <p className="text-xl text-red-600 font-semibold">{product.salePrice}</p>
+                                    <p className="text-sm text-gray-400 line-through">{product.price}</p>
+                                </div>
+                                <div className="flex items-center">
+                                    <Rating name="half-rating-read" defaultValue={product.rating} precision={0.5} readOnly />
+                                    <div className="text-xs text-gray-500 ml-3">(150)</div>
+                                </div>
+                            </div>
+                            <button className='block w-full py-1 text-center top-5 text-white bg-red-600 border border-red-600 rounded-b hover:bg-transparent hover:text-red-600 transition'>Add to Cart</button>
                         </div>
-                        <div className="flex items-center">
-                        <Rating name="half-rating-read" defaultValue={product.rating} precision={0.5} readOnly />
-                            <div className="text-xs text-gray-500 ml-3">(150)</div>
-                        </div>
-                    </div>
-                    <button className='block w-full py-1 text-center top-5 text-white bg-red-600 border border-red-600 rounded-b hover:bg-transparent hover:text-red-600 transition'>Add to Cart</button>
-            </div>
-        ))}
+                    ))}
             </div>
         </div>
     );
--- src/components/Pages/Banner/Banner.tsx
+++ src/components/Pages/Banner/Banner.tsx
@@ -33,10 +33,10 @@ export default function Banners() {
             <div className="grid md:grid-cols-2 xs:grid-cols-1">
               <div className="flex flex-col items-center md:items-start justify-center md:pl-15 lg:pl-32 p-12 text-center md:text-left">
                 <h1 className="text-4xl">
-                  T-shirts For men<span className="text-blue-500">(cotton)</span>
+                  Monster Beats Headphone<span className="text-blue-500">(Special)</span>
                 </h1>
                 <p className="text-2xl">
-                  Cotton t-shirt available for all size
+                  Best accessories just for you!
                 </p>
                 <p className="text-3xl">
                   $<span className="text-red-600">35</span>
@@ -50,7 +50,7 @@ export default function Banners() {
                 </NavLink>
               </div>
               <div className="md:-mt-0 lg:-mt-0 -mt-12">
-                <img src='https://media.gq-magazine.co.uk/photos/60af7335924d56e34d909e18/master/w_1920,h_1280,c_limit/Graphic%20tees_0002_New%20balance.jpg' alt="Image1" className="md:block" />
+                <img src='https://img.freepik.com/free-psd/creative-headphone-mockup_1310-744.jpg?w=740' alt="Image1" className="md:block" />
               </div>
             </div>
 
