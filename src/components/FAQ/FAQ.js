9bf75055a9264c48b52d0ac08ec8806bb5c928b0
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-02-18 11:48:41 +0600

    done

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -42,7 +42,7 @@ const Item = ({ title, children }) => {
 };
 export const Faq = () => {
   return (
-    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 overflow-x-hidden">
+    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
         <div className=" sm:text-center">
           <h2 className="text-4xl">Frequently Asked Question</h2>
