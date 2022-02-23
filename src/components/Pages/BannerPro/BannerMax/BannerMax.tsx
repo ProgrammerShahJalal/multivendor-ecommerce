01f1074a2ab88bc8e773ce619f47aa6c4ac80a80
ifaz5 <mutasim.ifaz@gmail.com>
2022-02-23 14:04:11 +0600

    css file removed

--- src/components/Pages/BannerPro/BannerMax/BannerMax.tsx
+++ src/components/Pages/BannerPro/BannerMax/BannerMax.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './BannerMax.css';
 const BannerMax = () => {
     
     return (
--- src/components/SpecialDeal/SpecialDeal.tsx
+++ src/components/SpecialDeal/SpecialDeal.tsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import CowndownTimer from "../CowntownTimer/CowndownTimer";
-import './SpecialDeal.css';
-
 interface DealState {
     deals: {
         img: string
