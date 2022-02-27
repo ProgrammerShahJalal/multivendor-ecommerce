64d1c9ce1bcbbb78cf8bcb2efd587076c163a353
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-02-27 10:37:49 +0600

    scroll

--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -9,9 +9,9 @@ export default class Shop extends PureComponent<Props> {
     render() {
         return (
             <div>
-                <ScrollToTop style={{display:'flex',alignItems:'center',justifyContent:'center'}} smooth color='#6f00ff' top={20} />
+                <ScrollToTop style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} smooth color='#6f00ff' top={20} />
                 <Products></Products>
-                <div style={{ display: 'flex', justifyContent: 'center', padding: 30 }}>
+                <div style={{ display: 'flex', justifyContent: 'center', padding: 30, backgroundColor: '#7209b7' }}>
                     <Pagination count={10} color="primary" />
                 </div>
 
