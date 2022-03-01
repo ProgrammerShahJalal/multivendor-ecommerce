f8ff90d1add3feed1fa50ca5b6d1e720f31c508d
ifaz5 <mutasim.ifaz@gmail.com>
2022-03-01 10:31:27 +0600

    uwu

--- src/components/SpecialDeal/DealDetails.js
+++ src/components/SpecialDeal/DealDetails.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { SwiperSlide, Swiper } from 'swiper/react';
+
 import UseAuth from '../../hooks/UseAuth';
 import Magnifier from "react-magnifier";
+import { SwiperSlide, Swiper } from 'swiper/react';
 import { FreeMode, Navigation, Thumbs } from "swiper";
 import "swiper/css";
 import "swiper/css/free-mode";
