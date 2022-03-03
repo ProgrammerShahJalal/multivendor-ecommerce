a96f404f43d85848dacfaedf322cfc9b772b09b7
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-03-03 06:48:22 +0600

    done

--- src/components/Pages/Team/Team.tsx
+++ src/components/Pages/Team/Team.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Typewriter } from 'react-simple-typewriter';
-import Celebrating from '../../Home/Celebrating';
 
 export interface IAppProps {
 }
@@ -9,7 +8,6 @@ export default class App extends React.PureComponent<IAppProps> {
   public render() {
     return (
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <Celebrating />
         <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
           <div>
             <h1 className="inline-block px-3 py-px mb-4 text-2xl font-semibold tracking-wider text-orange-900 uppercase rounded-full bg-teal-accent-400">
