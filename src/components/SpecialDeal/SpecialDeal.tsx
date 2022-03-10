55af95425d7efd51553260d38308a8387a064013
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-03-10 13:16:43 +0600

    deal

--- src/components/SpecialDeal/SpecialDeal.tsx
+++ src/components/SpecialDeal/SpecialDeal.tsx
@@ -59,9 +59,8 @@ const SpecialDeal: FC<SpecialProps> = ({ translate }) => {
         <Fragment>
             <div className="bg-gray-50">
                 <h2 className="text-3xl text-center font-extrabold text-gray-900 sm:text-4xl">
-                    {/* <span className="text-5xl pr-3" >Special</span>
-                    <span className=" text-indigo-600 text-5xl">Deal</span> */}
-                    <span className=" text-indigo-600 text-5xl">{translate('deal')}</span>
+                    <span className="text-5xl pr-3" >Special</span>
+                    <span className=" text-indigo-600 text-5xl">Deal</span>
                 </h2>
                 <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
                     <div className="mt-8 grid gap-4 lg:mt-0 grid-cols-1 justify-center items-center justify-self-center">
