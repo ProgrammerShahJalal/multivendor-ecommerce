82c9da7520751d50dd3f9df022243dc6da0c32fc
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-03-11 13:25:02 +0600

    cool

--- src/components/ProductView/ProductView.js
+++ src/components/ProductView/ProductView.js
@@ -47,10 +47,8 @@ const ProductView = ({ selectedProduct }) => {
 
 
     return (
-        // eslint-disable-next-line no-undef
-        <div onDoubleClick={handleClose} className='mx-auto'>
-            <div style={{ backgroundColor: 'white', width: '800px', height: '600px', overflow: 'scroll' }} className='py-12 container place-content-center px-8 justify-center items-center mx-auto mt-8 grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-6'>
-
+        <div className='mx-auto'>
+            <div className='py-12 border border-white container place-content-center px-8 justify-center items-centermt-8 grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-6'>
                 <div>
                     <Swiper
 
@@ -61,10 +59,16 @@ const ProductView = ({ selectedProduct }) => {
                         className="mySwiper2"
                     >
                         <SwiperSlide style={{ height: '300px', width: '300px' }}>
-                            <Magnifier src={selectedProduct.images[0]?.src} className='bg-cover select-none' />
+                            <Magnifier src={selectedProduct.img3} className='bg-cover select-none' />
                         </SwiperSlide>
                         <SwiperSlide style={{ height: '300px', width: '300px' }} >
-                            <Magnifier src={selectedProduct.images[1]?.src} className='bg-cover select-none' />
+                            <Magnifier src={selectedProduct.img} className='bg-cover select-none' />
+                        </SwiperSlide>
+                        <SwiperSlide style={{ height: '300px', width: '300px' }}>
+                            <Magnifier src={selectedProduct.hoverImg} className='bg-cover select-none' />
+                        </SwiperSlide>
+                        <SwiperSlide style={{ height: '300px', width: '300px' }} >
+                            <Magnifier src={selectedProduct.img4} className='bg-cover select-none' />
                         </SwiperSlide>
                     </Swiper>
                     <div className='mt-5'>
@@ -78,10 +82,16 @@ const ProductView = ({ selectedProduct }) => {
                             className="mySwiper"
                         >
                             <SwiperSlide style={{ height: '100px', width: '100px' }}>
-                                <img alt="" src={selectedProduct.images[1]?.src} className='bg-cover select-none cursor-pointer' />
+                                <img alt="" src={selectedProduct.img3} className='bg-cover select-none cursor-pointer' />
                             </SwiperSlide>
                             <SwiperSlide style={{ height: '100px', width: '100px' }} >
-                                <img alt="" src={selectedProduct.images[0]?.src} className='bg-cover select-none cursor-pointer' />
+                                <img alt="" src={selectedProduct.img} className='bg-cover select-none cursor-pointer' />
+                            </SwiperSlide>
+                            <SwiperSlide style={{ height: '100px', width: '100px' }}>
+                                <img alt="" src={selectedProduct.hoverImg} className='bg-cover select-none cursor-pointer' />
+                            </SwiperSlide>
+                            <SwiperSlide style={{ height: '100px', width: '100px' }}>
+                                <img alt="" src={selectedProduct.img4} className='bg-cover select-none cursor-pointer' />
                             </SwiperSlide>
                         </Swiper>
                     </div>
@@ -94,53 +104,56 @@ const ProductView = ({ selectedProduct }) => {
                             <Rating name="half-rating-read" defaultValue={6} precision={0.5} readOnly />
                         </div>
                         <div className="text-xs text-gray-500 ml-3">
-                            (1 Reviews)
+                            ({selectedProduct.rating} Reviews)
                         </div>
                     </div>
                     <div className='space-y-2'>
-                        <p className="p text-gray-800 font-semibold space-x-2">
+                        <p className="p text-gray-800 dark:text-white font-semibold space-x-2">
                             <span>Availability:</span>
                             <span className='text-green-600'>In stock</span>
                         </p>
                         <p className="space-x-2">
-                            <span className='text-gray-800 font-semibold'>Brand:</span>
+                            <span className='text-gray-800 dark:text-white font-semibold'>Brand:</span>
                             <span className='text-gray-600'>Nike</span>
                         </p>
                         <p className="space-x-2">
-                            <span className='text-gray-800 font-semibold'>Category:</span>
-                            <span className='text-gray-600'>Men</span>
+                            <span className='text-gray-800 dark:text-white font-semibold'>Category:</span>
+                            <span className='text-gray-600'></span>
                         </p>
                         <p className="space-x-2">
-                            <span className='text-gray-800 font-semibold'>SKU:</span>
+                            <span className='text-gray-800 dark:text-white font-semibold'>SKU:</span>
                             <span className='text-gray-600 uppercase'>{selectedProduct._id.slice(15, 25)}</span>
                         </p>
                     </div>
 
                     <div className="flex items-baseline mb-1 mt-2 space-x-2">
-                        <p className="text-xl text-indigo-500 font-semibold">{selectedProduct?.sale_price}</p>
-                        <p className="text-sm text-gray-400 line-through">{selectedProduct.reg_price}</p>
+                        <p className="text-xl text-indigo-500 font-semibold">{selectedProduct.salePrice}</p>
+                        <p className="text-sm text-gray-400 line-through">{selectedProduct.price}</p>
                     </div>
                     <div className="grid grid-cols-2">
                         <div className="">
-                            <h3 className="text-xl text-gray-800 mb-3 uppercase font-medium ">Color</h3>
+                            <h3 className="text-xl text-gray-800 dark:text-white mb-3 uppercase font-medium ">Color</h3>
                             <div className="flex gap-2">
                                 {/* Single Color Starts */}
                                 <div className="color-selctor">
-                                    <input type="radio" name='color' className='hidden' color='red' id='color-red' />
-                                    <label htmlFor="color" className='border border-gray-200 rounded-sm h-5 w-5 cursor-pointer shadow-sm block' style={{ backgroundColor: "red" }}></label>
+                                    <button className='w-5 h-5 bg-red-600 focus:ring-offset-2 focus:ring-1 focus:ring-indigo-500 focus:outline-none active:bg-red-700  '></button>
+                                    {/* <input type="radio" name='color' className='hidden' color='' id='color-red' />
+                                    <label htmlFor="color" className='border border-gray-200 rounded-sm h-5 w-5 cursor-pointer shadow-sm block bg-red-600'></label> */}
                                 </div>
                                 <div className="color-selctor">
-                                    <input type="radio" name='color' className='hidden' color='red' id='color-red' />
-                                    <label htmlFor="color" className='border border-gray-200 rounded-sm h-5 w-5 cursor-pointer shadow-sm block' style={{ backgroundColor: "green" }}></label>
+                                    {/* <input type="radio" name='color' className='hidden' color='red focus:ring-offset-2 focus:ring-1 focus:ring-sky-500  focus:outline-none  ' id='color-red' />
+                                    <label htmlFor="color" className='border border-gray-200 rounded-sm h-5 w-5 cursor-pointer shadow-sm block bg-green-600'></label> */}
+                                    <button className='w-5 h-5 bg-green-600 focus:ring-offset-2 focus:ring-1 focus:ring-indigo-500 focus:outline-none active:bg-green-700  '></button>
                                 </div>
                                 <div className="color-selctor">
-                                    <input type="radio" name='color' className='hidden' color='red' id='color-red' />
-                                    <label htmlFor="color" className='border border-gray-200 rounded-sm h-5 w-5 cursor-pointer shadow-sm block' style={{ backgroundColor: "blue" }}></label>
+                                    {/* <input type="radio" name='color' className='hidden' color='red focus:ring-offset-2 focus:ring-1 focus:ring-sky-500  focus:outline-none  ' id='color-red' />
+                                    <label htmlFor="color" className='border border-gray-200 rounded-sm h-5 w-5 cursor-pointer shadow-sm block bg-blue-500'></label> */}
+                                    <button className='w-5 h-5 bg-blue-600 focus:ring-offset-2 focus:ring-1 focus:ring-indigo-500 focus:outline-none active:bg-blue-700  '></button>
                                 </div>
                             </div>
                         </div>
                         <div className="pt-4 -mt-32 block">
-                            <h3 className="text-xl text-gray-800 mb-3 uppercase font-medium">Size</h3>
+                            <h3 className="text-xl text-gray-800 dark:text-white mb-3 uppercase font-medium">Size</h3>
                             <div className="flex item-center gap-2">
                                 <div className="size-selector">
                                     <input type="radio" name='size' className='hidden' id='xs' />
@@ -163,24 +176,26 @@ const ProductView = ({ selectedProduct }) => {
                             </div>
                         </div>
                         <div>
-                            <h3 className='text-xl text-gray-800 mb-1'>Quantity</h3>
+                            <h3 className='text-xl text-gray-800 dark:text-white mb-1'>Quantity</h3>
                             <div className="flex border border-gray-500 divide-gray-500 text-gray-600 divide-x w-max">
-                                <div className='h-8 w-8 flex items-center justify-center cursor-pointer select-none'>
+                                <button onClick={decrement} className='h-8 w-8 flex items-center justify-center cursor-pointer select-none'>
                                     -
-                                </div>
+                                </button>
                                 <div className='h-8 w-8 flex items-center justify-center'>
-                                    <input style={{ width: '34px', height: '34px' }} type="text" />
+                                    {counter}
                                 </div>
-                                <div className='h-8 w-8 flex items-center justify-center cursor-pointer select-none'>
+                                <button onClick={() => setCounter(counter + 1)} className='h-8 w-8 flex items-center justify-center cursor-pointer select-none'>
                                     +
-                                </div>
+                                </button>
                             </div>
                         </div>
-                        <div className="border-b -mt-20 border-gray-200 pb-5">
 
-                            <button className='w-40 text-center top-5 text-white  p-2 bg-indigo-500 border border-indigo-500 hover:bg-transparent hover:text-indigo-500 transition'>
-                                <i className="fa-regular fa-bag-shopping"></i> Add to cart
-                            </button>
+                        <div className="border-b -mt-20 border-gray-200 pb-5">
+                            <Link to="/">
+                                <button className='w-40 text-center top-5 text-white  p-2 bg-indigo-500 border border-indigo-500 hover:bg-transparent hover:text-indigo-500 transition'>
+                                    <i className="fa-regular fa-bag-shopping"></i> Add to cart
+                                </button>
+                            </Link>
                             <Link to="/">
                                 <button className='w-40 text-center top-5 hover:text-white my-2 p-2 hover:bg-indigo-500 border border-indigo-500 bg-transparent text-indigo-500 transition'>
                                     <i className="fa-regular fa-heart"> </i> Add to wishlist
@@ -188,6 +203,11 @@ const ProductView = ({ selectedProduct }) => {
                             </Link>
                         </div>
                     </div>
+                    {/* <div className='mt-2 flex gap-2'>
+                        <ShareButton {...shareFBButtonProps}><i class="fa-brands fa-facebook text-blue-600 text-2xl"></i></ShareButton>
+                        <ShareButton {...shareLNButtonProps}><i class="fa-brands fa-linkedin text-blue-500 text-2xl"></i></ShareButton>
+                        <ShareButton {...sharePNButtonProps}><i class="fa-brands fa-pinterest text-red-600 text-2xl"></i></ShareButton>
+                    </div> */}
                 </div>
             </div>
         </div>
