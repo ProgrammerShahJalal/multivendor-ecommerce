7ca7cdfe3ce1a8379d9a6f921659608efe0e0ef9
Karima-Aktari <karimaaktari911@gmail.com>
2022-03-14 17:16:32 +0600

    searchField

--- src/components/Pages/SearchField/SearchField.tsx
+++ src/components/Pages/SearchField/SearchField.tsx
@@ -4,7 +4,7 @@ import ProductSearchBar from '../ProductSearchBar/ProductSearchBar';
 const SearchField = () => {
     return (
         <div className="container mx-auto">
-            <h2 className='text-xl font font-extrabold tracking-tight sm:text-6xl text-center my-5 text-gray-400 dark:text-white'>Search Your Favourite Products Hear</h2>
+            <h2 className='text-3xl font font-extrabold text-center mt-3 text-gray-500 dark:text-white'>Search Your Favourite Products Here</h2>
             <ProductSearchBar />
         </div>
     );
