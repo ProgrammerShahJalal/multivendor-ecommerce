754684db7033854a43d618e75c5eae06c0e5c591
saharabanu <saharabanungn@gmail.com>
2022-03-15 19:49:15 +0600

    vendor api added

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,60 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home/Home";
-import HeaderLanguage from './components/Shared/HeaderLanguage';
-import Footer from './components/Shared/Footer/Footer';
-import Contact from './components/Contact/Contact.js';
-import AuthProvider from "./context/AuthProvider";
-import Register from "./components/Pages/RegisterPage/Register";
-import Login from "./components/Pages/LoginPage/Login";
-import NotFound from './components/NotFound/NotFound';
-import Team from './components/Pages/Team/Team';
-import Shop from "./components/Shop/Shop";
-import PrivateRoute from "./components/Route/PrivateRoute/PrivateRoute";
-import UserProfile from './components/UserProfile/UserProfile';
-import OrderTracking from './components/OrderTracking/OrderTracking';
-import Success from "./components/Success";
-import VendorShopPage from "./components/VenderShopPage/VenderShopPage";
-import VendorSidebar from "./components/VendorSidebar/VendorSidebar";
-import SpecialDeal from "./components/SpecialDeal/SpecialDeal";
-
-import Media from "./components/Dashboard/Media/Media";
-import AddProduct from "./components/Dashboard/AddProduct/AddProduct";
-import Categories from "./components/Dashboard/AddProduct/Sub/Categories/Categories";
-import Attributes from "./components/Dashboard/AddProduct/Sub/Attributes/Attributes";
-import DealDetails from "./components/SpecialDeal/DealDetails";
+import { useContext, useLayoutEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SingleProduct from "./components/AllProducts/SingleProduct";
 import DetailBlogPage from "./components/Blogs/DetailBlogPage";
+import Checkout from "./components/Checkout/Checkout";
+import Contact from './components/Contact/Contact.js';
+import CustomersService from "./components/CustomersService/CustomersService";
+import AddProduct from "./components/Dashboard/AddProduct/AddProduct";
+import Attributes from "./components/Dashboard/AddProduct/Sub/Attributes/Attributes";
+import Categories from "./components/Dashboard/AddProduct/Sub/Categories/Categories";
+// import { SummaryBoxSpecial } from "./components/Dashboard/DashboardHome/DashboardHome";
+import Products from "./components/Dashboard/AddProduct/Sub/Products/Products";
 import Affiliate from "./components/Dashboard/AffiliateDashboard/AffiliateDashboard/Affliate";
 import AffiliateLinks from "./components/Dashboard/AffiliateDashboard/AffiliateLinks/AffiliateLinks";
 import Dashboard from "./components/Dashboard/Dashboard/Dashboard";
-import { useLayoutEffect } from "react";
+import Media from "./components/Dashboard/Media/Media";
 import AllOrders from "./components/Dashboard/Order/allOrder";
+import DetailedOrder from "./components/Dashboard/Order/DetailedOrder";
+import Users from "./components/Dashboard/Users/Users";
 import Vendors from "./components/Dashboard/Vendors/Vendors";
 import VendorProfileDetails from "./components/Dashboard/Vendors/VendorsProfileDetails";
-// import { SummaryBoxSpecial } from "./components/Dashboard/DashboardHome/DashboardHome";
-import Products from "./components/Dashboard/AddProduct/Sub/Products/Products";
-import Users from "./components/Dashboard/Users/Users";
-import MenProductsDetail from './components/MenProductDetail';
-import WomenProductDetail from './components/WomenProductDetail';
+import Home from "./components/Home/Home";
 import KidsProductDetails from './components/KidsProductDetails';
-import Promo from './components/Promo/Promo';
-import UnitTesting from "./components/Pages/UnitTesting/UnitTesting";
-import WishList from "./components/Pages/WishList/WishList";
+import MenProductsDetail from './components/MenProductDetail';
+import NotFound from './components/NotFound/NotFound';
+import OrderTracking from './components/OrderTracking/OrderTracking';
 import About from "./components/Pages/About/About";
-import { useContext } from "react";
-import { LangContext } from "./components/Pages/MultiLanguage/languagecontext/lang";
-import Cart from "./components/Pages/CartPage/Cart";
-import Checkout from "./components/Checkout/Checkout";
-import AffiliateShop from "./components/Shop/AffiliateShop";
 import SiteUser from "./components/Pages/AddReview/SiteUser";
-import DetailedOrder from "./components/Dashboard/Order/DetailedOrder";
-import SingleProduct from "./components/AllProducts/SingleProduct";
+import Cart from "./components/Pages/CartPage/Cart";
+import Login from "./components/Pages/LoginPage/Login";
+import { LangContext } from "./components/Pages/MultiLanguage/languagecontext/lang";
+import Register from "./components/Pages/RegisterPage/Register";
 import SearchField from "./components/Pages/SearchField/SearchField";
+import Team from './components/Pages/Team/Team';
+import UnitTesting from "./components/Pages/UnitTesting/UnitTesting";
 import VendorInformations from "./components/Pages/VendorPages/VendorInformations";
 import VendorRegister from "./components/Pages/VendorPages/VendorRegister";
-import AdminRoute from "./components/Route/AdminRoute/AdminRoute";
-import StoreListCards from "./components/StoreListCard/StoreListCard";
-import TermsCondition from "./components/TermsCondition/TermsCondition";
-import CustomersService from "./components/CustomersService/CustomersService";
+import WishList from "./components/Pages/WishList/WishList";
 import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy";
+import Promo from './components/Promo/Promo';
+import AdminRoute from "./components/Route/AdminRoute/AdminRoute";
+import PrivateRoute from "./components/Route/PrivateRoute/PrivateRoute";
+import Footer from './components/Shared/Footer/Footer';
+import HeaderLanguage from './components/Shared/HeaderLanguage';
+import AffiliateShop from "./components/Shop/AffiliateShop";
+import Shop from "./components/Shop/Shop";
+import DealDetails from "./components/SpecialDeal/DealDetails";
+import SpecialDeal from "./components/SpecialDeal/SpecialDeal";
+import StoreListCards from "./components/StoreListCard/StoreListCard";
+import Success from "./components/Success";
+import TermsCondition from "./components/TermsCondition/TermsCondition";
+import UserProfile from './components/UserProfile/UserProfile';
+import VendorShopPage from "./components/VenderShopPage/VenderShopPage";
+import VendorSidebar from "./components/VendorSidebar/VendorSidebar";
+import WomenProductDetail from './components/WomenProductDetail';
+import AuthProvider from "./context/AuthProvider";
+
 
 
 
@@ -106,7 +105,7 @@ function App() {
             <Route path='/shop' element={<Shop />} />
             <Route path='/affiliateShop' element={<AffiliateShop />} />
             <Route path='/cart' element={<Cart />} />
-            <Route path='/vendorShop' element={<VendorShopPage />} />
+            <Route path='/vendorShop/:storeSlug' element={<VendorShopPage />} />
             <Route path='/vendorSidebar' element={<VendorSidebar />} />
             <Route path='/vendorLogin' element={<PrivateRoute><VendorInformations /></PrivateRoute>} />
             <Route path='/profile' element={<PrivateRoute><UserProfile /></PrivateRoute>} />
