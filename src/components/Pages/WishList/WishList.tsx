c1beab5f339e380d2a45746734f1bcd8d689b820
Karima-Aktari <karimaaktari911@gmail.com>
2022-03-15 16:46:15 +0600

    commit testing

--- src/components/Pages/WishList/WishList.tsx
+++ src/components/Pages/WishList/WishList.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
+
 const WishList = () => {
+    const dispatch: any = useDispatch()
+    const { wishlist } = useSelector((state: any) => state.wishlist)
+    // product.images[0]?.src
+    // onClick={() => dispatch(removeItem(item))}
     return (
         <div className="container mx-auto">
             <h1 className='text-4xl justify-center font-bold text-gray-500 uppercase py-4'>Wish List</h1>
@@ -17,21 +22,20 @@ const WishList = () => {
                     <h3>Remove</h3>
                 </div>
                 <div className='grid md:grid-cols-5 sm:grid-cols-1 gap-2 items-center justify-center  sm:text-green-600'>
-                    <img src='https://template.annimexweb.com/diva/assets/images/product-images/product-image3-1.jpg' className='w-full p-7 rounded' alt='' />
-                    <h3 className='font-bold text-xl text-pink-600'>Flying Ninja</h3>
-                    <h4 className='text-2xl text-gray-800 dark:text-white font-bold'>$500</h4>
-                    <button type='button' className="w-40 mx-auto text-white bg-purple-600 rounded-full hover:bg-pink-600 py-2">Add To Cart</button>
+                    {
+                        wishlist.map(product =>
+                        (<div>
+                            <img src={product.images[0]?.src} alt="" />
+                            <img src='https://template.annimexweb.com/diva/assets/images/product-images/product-image3-1.jpg' className='w-full p-7 rounded' alt='' />
+                            <h3 className='font-bold text-xl text-pink-600'>Flying Ninja</h3>
+                            <h4 className='text-2xl text-gray-800 dark:text-white font-bold'>$500</h4>
+                            <button type='button' className="w-40 mx-auto text-white bg-purple-600 rounded-full hover:bg-pink-600 py-2">Add To Cart</button>
 
-                    <button className='text-3xl text-rose-600 hover:text-gray-600'><i className="fa-solid fa-trash"></i></button>
+                            <button onClick={() => dispatch(removeItem(item))} className='text-3xl text-rose-600 hover:text-gray-600'><i className="fa-solid fa-trash"></i></button>
+                        </div>))
+                    }
                 </div>
-                <div className='grid  md:grid-cols-5 sm:grid-cols-1 gap-2 items-center justify-center  sm:text-green-600'>
-                    <img src='https://template.annimexweb.com/diva/assets/images/product-images/product-image3-1.jpg' className='w-full p-7 rounded' alt='' />
-                    <h3 className='font-bold text-xl text-pink-600'>Flying Ninja</h3>
-                    <h4 className='text-2xl text-gray-800 dark:text-white font-bold'>$500</h4>
-                    <button type='button' className="w-40 mx-auto text-white bg-purple-600 rounded-full hover:bg-pink-600 py-2">Add To Cart</button>
 
-                    <button className='text-3xl text-rose-600 hover:text-gray-600'><i className="fa-solid fa-trash"></i></button>
-                </div>
             </div>
 
         </div>
