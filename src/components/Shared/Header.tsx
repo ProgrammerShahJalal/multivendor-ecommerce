673529e70933b61b1623311d275b6c852be5b95b
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-03-15 11:01:42 +0600

    new style

--- src/components/Shared/Header.tsx
+++ src/components/Shared/Header.tsx
@@ -26,120 +26,120 @@ const navigation = {
     ],
 
     categories: [
-        // {
-        //     id: 'women',
-        //     name: 'Women',
-        //     featured: [
-        //         {
-        //             name: 'New Arrivals',
-        //             to: '#',
-        //             imageSrc: 'https://img.freepik.com/free-photo/three-young-beautiful-smiling-girls-trendy-summer-casual-jeans-clothes-sexy-carefree-women-posing-positive-models-sunglasses_158538-4730.jpg',
-        //             imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
-        //         },
-        //         {
-        //             name: 'Basic Tees',
-        //             to: '#',
-        //             imageSrc: 'https://img.freepik.com/free-photo/fashion-portrait-two-elegant-women-best-friends-posing-indoor-grey-wall-wearing-winter-fluffy-coat-black-casual-hat-fashionable-clothes-sisters-walking_273443-4074.jpg',
-        //             imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
-        //         },
-        //     ],
-        //     sections: [
-        //         {
-        //             id: 'clothing',
-        //             name: 'Clothing',
-        //             items: [
-        //                 { name: 'Tops', to: '/' },
-        //                 { name: 'Dresses', to: '/' },
-        //                 { name: 'Pants', to: '/' },
-        //                 { name: 'Denim', to: '/' },
-        //                 { name: 'Sweaters', to: '/' },
-        //                 { name: 'T-Shirts', to: '/' },
-        //                 { name: 'Jackets', to: '/' },
-        //                 { name: 'Activewear', to: '/' },
-        //                 { name: 'Browse All', to: '/' },
-        //             ],
-        //         },
-        //         {
-        //             id: 'accessories',
-        //             name: 'Accessories',
-        //             items: [
-        //                 { name: 'Watches', to: '/' },
-        //                 { name: 'Wallets', to: '/' },
-        //                 { name: 'Bags', to: '/' },
-        //                 { name: 'Sunglasses', to: '/' },
-        //                 { name: 'Hats', to: '/' },
-        //                 { name: 'Belts', to: '/' },
-        //             ],
-        //         },
-        //         {
-        //             id: 'brands',
-        //             name: 'Brands',
-        //             items: [
-        //                 { name: 'Full Nelson', to: '/' },
-        //                 { name: 'My Way', to: '/' },
-        //                 { name: 'Re-Arranged', to: '/' },
-        //                 { name: 'Counterfeit', to: '/' },
-        //                 { name: 'Significant Other', to: '/' },
-        //             ],
-        //         },
-        //     ],
-        // },
-        // {
-        //     id: 'men',
-        //     name: 'Men',
-        //     featured: [
-        //         {
-        //             name: 'New Arrivals',
-        //             to: '#',
-        //             imageSrc: 'https://img.freepik.com/free-photo/full-length-portrait-handsome-serious-man_171337-17388.jpg',
-        //             imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
-        //         },
-        //         {
-        //             name: 'Artwork Tees',
-        //             to: '#',
-        //             imageSrc: 'https://img.freepik.com/free-photo/elegant-young-handsome-man_1301-5870.jpg',
-        //             imageAlt:
-        //                 'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
-        //         },
-        //     ],
-        //     sections: [
-        //         {
-        //             id: 'clothing',
-        //             name: 'Clothing',
-        //             items: [
-        //                 { name: 'Tops', to: '/' },
-        //                 { name: 'Pants', to: '/' },
-        //                 { name: 'Sweaters', to: '/' },
-        //                 { name: 'T-Shirts', to: '/' },
-        //                 { name: 'Jackets', to: '/' },
-        //                 { name: 'Activewear', to: '/' },
-        //                 { name: 'Browse All', to: '/' },
-        //             ],
-        //         },
-        //         {
-        //             id: 'accessories',
-        //             name: 'Accessories',
-        //             items: [
-        //                 { name: 'Watches', to: '/' },
-        //                 { name: 'Wallets', to: '/' },
-        //                 { name: 'Bags', to: '/' },
-        //                 { name: 'Sunglasses', to: '/' },
-        //                 { name: 'Hats', to: '/' },
-        //                 { name: 'Belts', to: '/' },
-        //             ],
-        //         },
-        //         {
-        //             id: 'brands',
-        //             name: 'Brands',
-        //             items: [
-        //                 { name: 'Re-Arranged', to: '/' },
-        //                 { name: 'Counterfeit', to: '/' },
-        //                 { name: 'Full Nelson', to: '/' },
-        //                 { name: 'My Way', to: '/' },
-        //             ],
-        //         },
-        //     ],
-        // },
+        {
+            id: 'women',
+            name: 'Women',
+            featured: [
+                {
+                    name: 'New Arrivals',
+                    to: '#',
+                    imageSrc: 'https://img.freepik.com/free-photo/three-young-beautiful-smiling-girls-trendy-summer-casual-jeans-clothes-sexy-carefree-women-posing-positive-models-sunglasses_158538-4730.jpg',
+                    imageAlt: 'Models sitting back to back, wearing Basic Tee in black and bone.',
+                },
+                {
+                    name: 'Basic Tees',
+                    to: '#',
+                    imageSrc: 'https://img.freepik.com/free-photo/fashion-portrait-two-elegant-women-best-friends-posing-indoor-grey-wall-wearing-winter-fluffy-coat-black-casual-hat-fashionable-clothes-sisters-walking_273443-4074.jpg',
+                    imageAlt: 'Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.',
+                },
+            ],
+            sections: [
+                {
+                    id: 'clothing',
+                    name: 'Clothing',
+                    items: [
+                        { name: 'Tops', to: '/' },
+                        { name: 'Dresses', to: '/' },
+                        { name: 'Pants', to: '/' },
+                        { name: 'Denim', to: '/' },
+                        { name: 'Sweaters', to: '/' },
+                        { name: 'T-Shirts', to: '/' },
+                        { name: 'Jackets', to: '/' },
+                        { name: 'Activewear', to: '/' },
+                        { name: 'Browse All', to: '/' },
+                    ],
+                },
+                {
+                    id: 'accessories',
+                    name: 'Accessories',
+                    items: [
+                        { name: 'Watches', to: '/' },
+                        { name: 'Wallets', to: '/' },
+                        { name: 'Bags', to: '/' },
+                        { name: 'Sunglasses', to: '/' },
+                        { name: 'Hats', to: '/' },
+                        { name: 'Belts', to: '/' },
+                    ],
+                },
+                {
+                    id: 'brands',
+                    name: 'Brands',
+                    items: [
+                        { name: 'Full Nelson', to: '/' },
+                        { name: 'My Way', to: '/' },
+                        { name: 'Re-Arranged', to: '/' },
+                        { name: 'Counterfeit', to: '/' },
+                        { name: 'Significant Other', to: '/' },
+                    ],
+                },
+            ],
+        },
+        {
+            id: 'men',
+            name: 'Men',
+            featured: [
+                {
+                    name: 'New Arrivals',
+                    to: '#',
+                    imageSrc: 'https://img.freepik.com/free-photo/full-length-portrait-handsome-serious-man_171337-17388.jpg',
+                    imageAlt: 'Drawstring top with elastic loop closure and textured interior padding.',
+                },
+                {
+                    name: 'Artwork Tees',
+                    to: '#',
+                    imageSrc: 'https://img.freepik.com/free-photo/elegant-young-handsome-man_1301-5870.jpg',
+                    imageAlt:
+                        'Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.',
+                },
+            ],
+            sections: [
+                {
+                    id: 'clothing',
+                    name: 'Clothing',
+                    items: [
+                        { name: 'Tops', to: '/' },
+                        { name: 'Pants', to: '/' },
+                        { name: 'Sweaters', to: '/' },
+                        { name: 'T-Shirts', to: '/' },
+                        { name: 'Jackets', to: '/' },
+                        { name: 'Activewear', to: '/' },
+                        { name: 'Browse All', to: '/' },
+                    ],
+                },
+                {
+                    id: 'accessories',
+                    name: 'Accessories',
+                    items: [
+                        { name: 'Watches', to: '/' },
+                        { name: 'Wallets', to: '/' },
+                        { name: 'Bags', to: '/' },
+                        { name: 'Sunglasses', to: '/' },
+                        { name: 'Hats', to: '/' },
+                        { name: 'Belts', to: '/' },
+                    ],
+                },
+                {
+                    id: 'brands',
+                    name: 'Brands',
+                    items: [
+                        { name: 'Re-Arranged', to: '/' },
+                        { name: 'Counterfeit', to: '/' },
+                        { name: 'Full Nelson', to: '/' },
+                        { name: 'My Way', to: '/' },
+                    ],
+                },
+            ],
+        },
     ],
 }
 
