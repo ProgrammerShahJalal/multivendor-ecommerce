dd818305ba4a759a9137a18e13cb1c770091fb64
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-03-19 10:01:45 +0600

    done

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -11,7 +11,7 @@ import Brands from '../Pages/Brands/Brands';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 import Notify from '../Notify/Notify';
 import PaginatedBlogsHome from '../Pages/PaginatedBlogsItems/PaginatedBlogsHome';
-// import Products from '../Products/Products';
+import Products from '../Products/Products';
 // import Alan from './Alan-AI/Alan';
 import Reviews from '../Pages/Reviews/Reviews';
 import SpecialDeal from './../SpecialDeal/SpecialDeal';
@@ -40,7 +40,7 @@ export default function Home() {
             <TopBanner />
             <SpecialDeal translate={translate} />
             <BannerGadget />
-            {/* <Products translate={translate} /> */}
+            <Products translate={translate} />
             {/* <AllProducts translate={translate} /> */}
             <BannerPro />
             <HomeProducts translate={translate} />
