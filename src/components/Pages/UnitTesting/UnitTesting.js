c1270774f750f9e8ca7fa6ffd07285b4a359b3b4
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-03-23 21:22:29 +0600

    fire on

--- src/components/Pages/UnitTesting/UnitTesting.js
+++ src/components/Pages/UnitTesting/UnitTesting.js
@@ -6,10 +6,12 @@ const UnitTesting = () => {
         <>
             <div className='w-96 px-5 mx-auto'>
                 <h2 className=" py-3 text-3xl text-center font-extrabold text-gray-900 dark:text-white sm:text-4xl">
-                    <span className="text-5xl pr-3" >Unit</span>
-                    <span className=" text-indigo-600 text-5xl">Testing</span>
+                    <span className="lg:text-5xl md:text-3xl sm:text-lg pr-3" >Unit</span>
+                    <span className=" text-indigo-600 dark:text-white lg:text-5xl md:text-3xl sm:text-lg">Testing</span>
                 </h2>
-                <lottie-player src="https://assets8.lottiefiles.com/packages/lf20_aj6xq4lp.json" background="transparent" speed="1" style={{ width: '600px', height: '600px' }} loop autoplay></lottie-player>
+                <div className='lg:w-96 md:w-72 sm:w-28'>
+                    <lottie-player src="https://assets8.lottiefiles.com/packages/lf20_aj6xq4lp.json" background="transparent" speed="1" loop autoplay></lottie-player>
+                </div>
             </div>
             <Counter />
         </>
